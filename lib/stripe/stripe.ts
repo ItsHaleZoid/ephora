7ac0cd9f043b75ba2bfa917c520e107c9d44dd95
HaleZoid <93285320+ItsHaleZoid@users.noreply.github.com>
2025-04-21 23:44:28 +0500

    stripe api deleted

--- lib/stripe/stripe.ts
+++
@@ -1,9 +0,0 @@
-// lib/stripe/stripe.ts
-import Stripe from 'stripe'
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  // Only use officially supported versions here
- apiVersion: '2025-03-31.basil',
-})
-
-export { stripe }
