a95a933d8a6de34b01292a3ca443877651709897
HaleZoid <93285320+ItsHaleZoid@users.noreply.github.com>
2025-04-21 23:42:44 +0500

    basil fix

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   // Only use officially supported versions here
- apiVersion: '2025-03-31',
+ apiVersion: '2025-03-31.basil',
 })
 
 export { stripe }
